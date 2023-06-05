de3316e381b8e9f58bef233752fdf90f3dd25be5
Donquixote <826714738@qq.com>
2023-06-05 23:23:20 +0800

    集成gin

--- main.go
+++ main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello GO!")
+	r := gin.New()
+	r.Use(gin.Logger(), gin.Recovery())
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"Hello": "World!",
+		})
+	})
+
+	r.NoRoute(func(ctx *gin.Context) {
+		acceptString := ctx.Request.Header.Get("Accetpt")
+		if strings.Contains(acceptString, "text/html") {
+			ctx.String(http.StatusNotFound, "404")
+		} else {
+			ctx.JSON(http.StatusNotFound, gin.H{
+				"error_code":    404,
+				"error_message": "路由未定义，请确认 url 和请求方法是否正确。",
+			})
+		}
+	})
+
+	r.Run(":8000")
 }
