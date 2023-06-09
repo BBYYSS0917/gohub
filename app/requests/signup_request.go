2532ed2e6d12fab38e1ae2b06c0ddc70d3bb3774
Donquixote <826714738@qq.com>
2023-06-09 14:39:18 +0800

    JSON 解析和验证器封装

--- app/requests/signup_request.go
+++ app/requests/signup_request.go
@@ -25,16 +25,7 @@ func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]str
 		},
 	}
 
-	// 配置初始化
-	opts := govalidator.Options{
-		Data:          data,
-		Rules:         rules,
-		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
-		Messages:      messages,
-	}
-
-	// 开始验证
-	return govalidator.New(opts).ValidateStruct()
+	return validate(data, rules, messages)
 }
 
 type SignupEmailExistRequest struct {
@@ -58,14 +49,18 @@ func ValidateSignupEmailExist(data interface{}, c *gin.Context) map[string][]str
 		},
 	}
 
-	// 配置初始化
-	opts := govalidator.Options{
-		Data:          data,
-		Rules:         rules,
-		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
-		Messages:      messages,
-	}
-
-	// 开始验证
-	return govalidator.New(opts).ValidateStruct()
+	return validate(data, rules, messages)
 }
+
+// func validate(data interface{}, rules govalidator.MapData, messages govalidator.MapData) map[string][]string {
+// 	// 配置选项
+// 	opts := govalidator.Options{
+// 		Data:          data,
+// 		Rules:         rules,
+// 		TagIdentifier: "valid", // 模型中的 Struct 标签标识符
+// 		Messages:      messages,
+// 	}
+
+// 	// 开始验证
+// 	return govalidator.New(opts).ValidateStruct()
+// }
