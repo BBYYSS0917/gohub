3f7b36ebad91f1e2e0b67266d0b9e3c673d9c571
Donquixote <826714738@qq.com>
2023-06-16 10:16:15 +0800

    手机号+短信验证码注册

--- routes/api.go
+++ routes/api.go
@@ -21,6 +21,9 @@ func RegisterAPIRoutes(r *gin.Engine) {
 			//判断Email是否已注册
 			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
 
+			authGroup.POST("/signup/email/exist", suc.IsEmailExist)
+			authGroup.POST("/signup/using-phone", suc.SignupUsingPhone)
+
 			// 发送验证码
 			vcc := new(auth.VerifyCodeController)
 			// 图片验证码，需要加限流
